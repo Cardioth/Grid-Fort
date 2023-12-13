40a9a5d7e9b9b26a7f4dca80bde35c914cc66c36
Cardioth <flashkid@hotmail.com>
2023-12-14 03:15:24 +1000

    style sheet 404 issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ export default {
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return 'style.css';
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
   },
 };
