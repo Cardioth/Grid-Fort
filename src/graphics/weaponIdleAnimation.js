ae76c057141b1470f07ecf233a8aa208344c427d
Cardioth <flashkid@hotmail.com>
2023-12-24 03:54:00 +1000

    improved weapon idle rotation animation

--- src/graphics/weaponIdleAnimation.js
+++ src/graphics/weaponIdleAnimation.js
@@ -3,15 +3,23 @@ import { scene } from "./sceneInitialization";
 
 // Weapon idle animation, picks a random rotation and rotates weapon to that rotation
 export function weaponIdleAnimation(weapon) {
-    const rotation = Math.random() * 2 * Math.PI;
+    const targetRotation = Math.random() * 2 * Math.PI;
+    let deltaRotation = targetRotation - weapon.rotation.z;
+
+    if (deltaRotation > Math.PI) {
+        deltaRotation -= 2 * Math.PI;
+    } else if (deltaRotation < -Math.PI) {
+        deltaRotation += 2 * Math.PI;
+    }
+
+    const finalRotation = weapon.rotation.z + deltaRotation;
 
     const animation = new BABYLON.Animation("weaponIdle", "rotation.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
     animation.setKeys([
         { frame: 0, value: weapon.rotation.z },
-        { frame: 90, value: rotation }
+        { frame: 90, value: finalRotation }
     ]);
 
-    // Add animations to children meshes
     for(const child of weapon.getChildren()){
         child.animations = [];
         child.animations.push(animation);
