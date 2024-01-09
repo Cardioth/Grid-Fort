73b3c783fad07247919336c86d725488bd0dd273
Cardioth <flashkid@hotmail.com>
2024-01-09 11:53:20 +1000

    update server url

--- client/src/app.js
+++ client/src/app.js
@@ -5,14 +5,14 @@ import io from 'socket.io-client';
 
 // Determine the environment and set the server URL accordingly
 const isDevelopment = window.location.hostname === 'localhost';
-const serverUrl = isDevelopment ? 'http://localhost:3000' : 'https://your-production-server.com';
+const serverUrl = isDevelopment ? 'http://localhost:3000' : 'https://grid-fort-server.herokuapp.com';
 
 // Connect to the server
 const socket = io(serverUrl);
 
 // Event listener for successful connection
 socket.on('connect', () => {
-  console.log('Connected to the server!');
+  console.log('Connected to the server!' + serverUrl);
 });
 
 // Handle the event sent from the server
