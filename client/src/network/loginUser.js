3ef797049696f5f3e93fd4ba19a3495a2a106b6c
Cardioth <flashkid@hotmail.com>
2024-01-12 07:43:10 +1000

    login UI fix 2

--- client/src/network/loginUser.js
+++ client/src/network/loginUser.js
@@ -1,6 +1,7 @@
 import { setCurrentScene } from "../managers/sceneManager";
 import { fadeToBlack } from "../ui/generalGUI";
 import { connectToServer } from "./connect";
+import { createAuthMessage } from "./createAuthMessage";
 import { unhideLoginButtons } from "./loginInterface";
 import { serverUrl } from "./serverURL";
 
@@ -25,7 +26,7 @@ export function loginUser(username, password) {
         }
     })
     .catch(error => {
-      unhideLoginButtons();
+      createAuthMessage("Login failed", unhideLoginButtons);
       console.error('Login error:', error);
     });
   }
