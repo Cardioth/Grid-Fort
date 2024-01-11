ce1ee1f4d2a7e7e7f6b50ef8f5d0ab559935612c
Cardioth <flashkid@hotmail.com>
2024-01-11 20:13:23 +1000

    fix trailing slash

--- client/src/network/serverURL.js
+++ client/src/network/serverURL.js
@@ -1,2 +1,2 @@
 const isDevelopment = window.location.hostname === 'localhost';
-export const serverUrl = isDevelopment ? 'http://localhost:3000' : 'https://grid-fort-server-d1b61f740a9b.herokuapp.com/';
+export const serverUrl = isDevelopment ? 'http://localhost:3000' : 'https://grid-fort-server-d1b61f740a9b.herokuapp.com';
